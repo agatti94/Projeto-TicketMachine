415f1bbda0d7d47ce7dd7a9a6587c71e17fed1be
ARTHUR FABRIS GATTI <unifagatti>
2022-09-30 22:27:39 -0300

    Adicionando testes unitarios

---
+++ Source Code Inspection/src/br/calebe/ticketmachine/tests/TestTroco.java
@@ -0,0 +1,5 @@
+package br.calebe.ticketmachine.tests;
+
+public class TestTroco {
+    
+}
---
+++ Source Code Inspection/src/br/calebe/ticketmachine/tests/TicketMachineTest.java
@@ -0,0 +1,45 @@
+package br.calebe.ticketmachine.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.TestCouldNotBeSkippedException;
+
+import br.calebe.ticketmachine.core.TicketMachine;
+
+public class TicketMachineTest {
+    @Test
+    public void testInserirUmaNota()
+    {
+        TicketMachine ticket = new TicketMachine(1);
+        int quantidade = 1;
+        try{
+            ticket.inserir(quantidade);
+
+            assertEquals(2, ticket.getSaldo());
+        }
+        catch(Exception e){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testImprimeValor(){
+        TicketMachine ticket = new TicketMachine(0);
+        int valor = 0;
+        String result = "*****************\n";
+        result += "*** R$ " + valor + ",00 ****\n";
+        result += "*****************\n";
+        try{
+            String a = ticket.imprimir();
+            assertEquals(result, a);
+        }
+        catch(Exception e){
+            Assert.fail();
+        }
+    }
+
+}
