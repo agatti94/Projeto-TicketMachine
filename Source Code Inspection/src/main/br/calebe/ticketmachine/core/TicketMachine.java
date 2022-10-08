d4b29255e2971f8f8c75452b40c1d8787b6aabfc
ARTHUR FABRIS GATTI <unifagatti>
2022-10-07 22:13:23 -0300

    mudando

---
+++ Source Code Inspection/src/main/br/calebe/ticketmachine/core/TicketMachine.java
@@ -0,0 +1,52 @@
+package br.calebe.ticketmachine.core;
+
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class TicketMachine {
+
+    protected int valor;
+    protected int saldo;
+    protected int[] papelMoeda = { 2, 5, 10, 20, 50, 100 };
+
+    public TicketMachine(int valor) {
+        this.valor = valor;
+        this.saldo = 0;
+    }
+
+    public void inserir(int quantia) throws PapelMoedaInvalidaException {
+        boolean achou = false;
+        for (int i = 0; i < papelMoeda.length && !achou; i++) {
+            if (papelMoeda[0] == quantia || papelMoeda[1] == quantia || papelMoeda[2] == quantia || papelMoeda[3] == quantia || papelMoeda[4] == quantia || papelMoeda[5] == quantia) {
+                achou = true;
+            }
+        }
+        if (!achou) {
+            throw new PapelMoedaInvalidaException("Valor não encontrado");
+        }
+        this.saldo += quantia;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public PapelMoeda[] getTroco() {
+        return Troco.getPapeisMoeda();
+    }
+
+    public String imprimir() throws SaldoInsuficienteException {
+        if (saldo < valor) {
+            throw new SaldoInsuficienteException("Valor menor do que o permitido");
+        }
+        String result = "*****************\n";
+        result += "*** R$ " + valor + ",00 ****\n";
+        result += "*****************\n";
+        return result;
+    }
+}
---
+++ Source Code Inspection/src/main/br/calebe/ticketmachine/core/Troco.java
@@ -0,0 +1,68 @@
+package br.calebe.ticketmachine.core;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+class Troco{
+
+    static protected PapelMoeda[] papeisMoeda;
+
+    public Troco(int valor) {
+        papeisMoeda = new PapelMoeda[6];
+        papeisMoeda[5] = new PapelMoeda(100, (valor/100));
+        valor = valor - (valor%100);
+        papeisMoeda[4] = new PapelMoeda(50, (valor/50));
+        valor = valor - (valor%50);
+        papeisMoeda[3] = new PapelMoeda(20, (valor/20));
+        valor = valor - (valor%20);
+        papeisMoeda[2] = new PapelMoeda(10, (valor/10));
+        valor = valor - (valor%10);
+        papeisMoeda[1] = new PapelMoeda(5, (valor/5));
+        valor = valor - (valor%5);
+        papeisMoeda[0] = new PapelMoeda(2, (valor/2));
+        valor = valor - (valor%2);
+    }
+
+    public Iterator<PapelMoeda> getIterator() {
+        return new TrocoIterator(this);
+    }
+
+    public static PapelMoeda[] getPapeisMoeda(){
+        return papeisMoeda;
+    }
+
+    class TrocoIterator implements Iterator<PapelMoeda> {
+
+        protected Troco troco;
+
+        public TrocoIterator(Troco troco) {
+            this.troco = troco;
+        }
+
+        @Override
+        public boolean hasNext() {
+            for (int i = 5; i >= 0; i++) {
+                if (troco.papeisMoeda[i] != null) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public PapelMoeda next() {
+            PapelMoeda ret = null;
+            for (int i = 5; i >= 0 && ret != null; i++) {
+                if (troco.papeisMoeda[i] != null) {
+                    ret = troco.papeisMoeda[i];
+                    troco.papeisMoeda[i] = null;
+                }
+            }
+            return ret;
+        }
+
+    }
+}
